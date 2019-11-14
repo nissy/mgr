eab2a9f168e194b7fea367cff39aaa62a8ace6c0
nissy <ni@sheeda.jp>
2019-11-14 15:26:34 +0900

    fix time renge

--- redis.go
+++ redis.go
@@ -3,27 +3,34 @@ package mgr
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/nissy/mgr/decoder"
 )
 
+var now = time.Now()
+
 type ToRedis struct {
-	SourceFile    string          `yaml:"source_file"`
-	Address       string          `yaml:"address"`
-	Migrates      []*MigrateRedis `yaml:"migrates"`
-	conn          redis.Conn
-	toDB          int
-	isTo          bool
-	decoderExpiry int64
-	decoderDB     int
-	decoderList   int
+	SourceFile          string          `yaml:"source_file"`
+	Address             string          `yaml:"address"`
+	Migrates            []*MigrateRedis `yaml:"migrates"`
+	conn                redis.Conn
+	toDB                int
+	toExpireMinSec      int64
+	toExpireMaxSec      int64
+	isToDB              bool
+	decoderExpiryUnixMs int64
+	decoderDB           int
+	decoderList         int
 	decoder.Nop
 }
 
 type MigrateRedis struct {
-	SourceDB int `yaml:"source_db"`
-	ToDB     int `yaml:"to_db"`
+	SourceDB       int   `yaml:"source_db"`
+	ToDB           int   `yaml:"to_db"`
+	ToExpireMinSec int64 `yaml:"to_expire_min_sec"`
+	ToExpireMaxSec int64 `yaml:"to_expire_max_sec"`
 }
 
 func (t *ToRedis) Do() (err error) {
@@ -41,14 +48,16 @@ func (t *ToRedis) Do() (err error) {
 
 func (t *ToRedis) StartDatabase(n int, offset int) error {
 	t.decoderDB = n
-	t.isTo = false
+	t.isToDB = false
 	for _, v := range t.Migrates {
 		if t.decoderDB == v.SourceDB {
 			if _, err := t.conn.Do("SELECT", v.ToDB); err != nil {
 				return err
 			}
 			t.toDB = v.ToDB
-			t.isTo = true
+			t.isToDB = true
+			t.toExpireMinSec = v.ToExpireMinSec
+			t.toExpireMaxSec = v.ToExpireMaxSec
 			return nil
 		}
 	}
@@ -56,38 +65,59 @@ func (t *ToRedis) StartDatabase(n int, offset int) error {
 }
 
 func (t *ToRedis) exec(cmd string, key []byte, args ...interface{}) (err error) {
-	if t.isTo {
-		if _, err = t.conn.Do(cmd, append([]interface{}{key}, args...)...); err == nil {
-			fmt.Printf("SOURCE=%v TO=%v %s %s\n", t.decoderDB, t.toDB, cmd, key)
-		}
+	if _, err = t.conn.Do(cmd, append([]interface{}{key}, args...)...); err == nil {
+		fmt.Printf("SOURCE=%v TO=%v %s %s\n", t.decoderDB, t.toDB, cmd, key)
 	}
 	return err
 }
 
 func (t *ToRedis) execExpire(key []byte) error {
-	if t.decoderExpiry > 0 {
-		if _, err := t.conn.Do("EXPIRE", key, t.decoderExpiry); err != nil {
+	if t.decoderExpiryUnixMs > 0 {
+		if _, err := t.conn.Do("EXPIREAT", key, int(t.decoderExpiryUnixMs/1000)); err != nil {
 			return err
 		}
-		t.decoderExpiry = 0
+		t.decoderExpiryUnixMs = 0
 	}
 	return nil
 }
 
+func (t *ToRedis) isNotSend(expiryUnixMs int64) bool {
+	exUnixNano := time.Unix(expiryUnixMs/1000, 0).UnixNano()
+	minUnixNano := now.Add(time.Duration(t.toExpireMinSec) * time.Second).UnixNano()
+	maxUnixNano := now.Add(time.Duration(t.toExpireMaxSec) * time.Second).UnixNano()
+	if t.toExpireMinSec > 0 {
+		if minUnixNano > exUnixNano {
+			return true
+		}
+	}
+	if t.toExpireMaxSec > 0 {
+		if maxUnixNano < exUnixNano {
+			return true
+		}
+	}
+	return !t.isToDB
+}
+
 func (t *ToRedis) Set(key, value []byte, expiry int64) error {
+	if t.isNotSend(expiry) {
+		return nil
+	}
 	if err := t.exec("SET", key, value); err != nil {
 		return err
 	}
-	t.decoderExpiry = expiry
+	t.decoderExpiryUnixMs = expiry
 	return t.execExpire(key)
 }
 
 func (t *ToRedis) StartHash(key []byte, length, expiry int64) (err error) {
-	t.decoderExpiry = expiry
+	t.decoderExpiryUnixMs = expiry
 	return err
 }
 
 func (t *ToRedis) Hset(key, field, value []byte) error {
+	if t.isNotSend(t.decoderExpiryUnixMs) {
+		return nil
+	}
 	if err := t.exec("HSET", key, field, value); err != nil {
 		return err
 	}
@@ -95,11 +125,14 @@ func (t *ToRedis) Hset(key, field, value []byte) error {
 }
 
 func (t *ToRedis) StartSet(key []byte, cardinality, expiry int64) (err error) {
-	t.decoderExpiry = expiry
+	t.decoderExpiryUnixMs = expiry
 	return err
 }
 
 func (t *ToRedis) Sadd(key, member []byte) error {
+	if t.isNotSend(t.decoderExpiryUnixMs) {
+		return nil
+	}
 	if err := t.exec("SADD", key, member); err != nil {
 		return err
 	}
@@ -108,12 +141,15 @@ func (t *ToRedis) Sadd(key, member []byte) error {
 
 func (t *ToRedis) StartList(key []byte, length, expiry int64) error {
 	t.decoderList = 0
-	t.decoderExpiry = expiry
+	t.decoderExpiryUnixMs = expiry
 	return nil
 }
 
 func (t *ToRedis) Rpush(key, value []byte) error {
 	t.decoderList++
+	if t.isNotSend(t.decoderExpiryUnixMs) {
+		return nil
+	}
 	if err := t.exec("RPUSH", key, value); err != nil {
 		return err
 	}
@@ -122,12 +158,15 @@ func (t *ToRedis) Rpush(key, value []byte) error {
 
 func (t *ToRedis) StartZSet(key []byte, cardinality, expiry int64) error {
 	t.decoderList = 0
-	t.decoderExpiry = expiry
+	t.decoderExpiryUnixMs = expiry
 	return nil
 }
 
 func (t *ToRedis) Zadd(key []byte, score float64, member []byte) error {
 	t.decoderList++
+	if t.isNotSend(t.decoderExpiryUnixMs) {
+		return nil
+	}
 	if err := t.exec("ZADD", key, score, member); err != nil {
 		return err
 	}
